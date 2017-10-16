5db6efb6c063855573dc9e9cadd70ac289f86a93
almack131216 <al_2003_1@hotmail.com>
2017-10-16 19:50:05 +0200

    amcust | page-title added to post (top + bottom)

--- wp-content/themes/kleo/functions.php
+++ wp-content/themes/kleo/functions.php
@@ -904,7 +904,7 @@ if ( ! function_exists( 'kleo_title_main_content' ) ) {
 					$title = kleo_title();
 				}
 
-				echo '<div class="container">';
+				echo '<div class="container amcust_page-title-container">';
 				echo '<h1 class="page-title">' . $title . '</h1>';
 				echo '</div>';
 			}
