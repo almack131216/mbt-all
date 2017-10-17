e3ac12caa64300acd717dd81b78dcb6286a4deaa
almack131216 <al_2003_1@hotmail.com>
2017-10-17 23:33:37 +0200

    amcust | custom post / tag titles

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -11,8 +11,24 @@
  * Add custom code below
 */
 
+global $amCustStringPrefixTitleTag;
+global $amCustStringPrefixTitleCategory;
+
+$amCustStringPrefixTitleTag = 'Tagged: ';
+$amCustStringPrefixTitleCategory = '';
+
 @ini_set( 'upload_max_size' , '64M' );
 @ini_set( 'post_max_size', '64M');
 @ini_set( 'max_execution_time', '300' );
 
 define('WP_SCSS_ALWAYS_RECOMPILE', true);
+
+function avia_fix_breadcrumb_trail($trail)
+{
+	foreach($trail as $key => $data)
+	{
+		if(strpos($data, 'Auto Draft') !== false) unset($trail[$key]);
+	}
+	return $trail;
+}
+add_filter('avia_breadcrumbs_trail','avia_fix_breadcrumb_trail');
