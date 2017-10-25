8863b07212c7d6ab00899a44c018b873ecf73043
almack131216 <al_2003_1@hotmail.com>
2017-10-26 00:17:50 +0200

    THEME CHILD | custom content UI + thumbnails

--- wp-content/themes/kleo-child/content.php
+++ wp-content/themes/kleo-child/content.php
@@ -11,6 +11,7 @@
 ?>
 
 <?php
+$amcust_debug_content = false;
 /* Helper variables for this template */
 $is_single          = is_single();
 $post_meta_enabled  = kleo_postmeta_enabled();
@@ -64,11 +65,17 @@ if ( $is_single && get_cfield( 'centered_text' ) == 1 ) {
 		<div class="container amcust_page-title-container amcust-title-below-media">
 			<h1 class="page-title"><?php the_title(); ?></h1>
 		</div>
+		
+		<?php if($amcust_debug_content) echo '<h6>do_action( \'kleo_before_inner_article_loop\' )</h6>'; ?>
 		<?php do_action( 'kleo_before_inner_article_loop' ); ?>
 
+		<?php if($amcust_debug_content) echo '<h6>if ( ! $is_single )</h6>'; ?>
 		<?php if ( ! $is_single ) : // Only display Excerpts for Search ?>
 
+			<?php if($amcust_debug_content) echo '<h6>kleo_excerpt( 50 )</h6>'; ?>
 			<?php echo kleo_excerpt( 50 ); ?>
+			
+			<?php if($amcust_debug_content) echo '<h6>kleo-continue</h6>'; ?>
 			<p class="kleo-continue">
 				<a class="btn btn-default"
 				   href="<?php the_permalink() ?>">
@@ -76,16 +83,95 @@ if ( $is_single && get_cfield( 'centered_text' ) == 1 ) {
 				</a>
 			</p>
 
+			
 		<?php else : ?>
-
-			<?php the_content( esc_html__( 'Continue reading <span class="meta-nav">&rarr;</span>', 'kleo_framework' ) ); ?>
+			<?php if($amcust_debug_content) echo '<h6>(OLD) the_content</h6>'; ?>
+			<?php
+				// the_content( esc_html__( 'Continue reading <span class="meta-nav">&rarr;</span>', 'kleo_framework' ) );
+			?>
+			<?php if($amcust_debug_content) echo '<h6>wp_link_pages</h6>'; ?>
 			<?php wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kleo_framework' ),
 				'after'  => '</div>',
 			) ); ?>
+			
+			<?php if($amcust_debug_content) echo '<h6>START cust...</h6>'; ?>
+			
+<?php 
+// $meta_print_value=get_post_meta(get_the_ID(),'',true);
+// print_r($meta_print_value);
+// echo "<h6>book_author: " + $post_meta_enabled + "</h6>";
+
+function array_push_assoc($array, $key, $value){
+	$array[$key] = $value;
+	return $array;
+}
+
+function filter_gpm($array){
+	$mk = array();
+	foreach($array as $k => $v){
+		if(is_array($v) && count($v) == 1){
+		$mk = array_push_assoc($mk, $k, $v[0]);
+	} else {
+		$mk = array_push_assoc($mk, $k, $v);
+		}
+	}
+	return $mk;
+}
+
+$mk = filter_gpm( get_post_meta( get_the_ID() ) );
+//print_r($mk);
+
+// check if the post has a Post Thumbnail assigned to it.
+// if ( has_post_thumbnail() ) {
+// 	the_post_thumbnail();
+// } 
+
+$post_type = get_post_type($post_id);
+echo '<p><strong>Post Type: </strong>' . $post->post_type . '</p>';
+echo '<p><strong>Post ID: </strong>' . get_the_ID() . '</p>';
+echo '<p><strong>Book Author: </strong>' . $mk['book_author'] . '</p>';
+echo '<p><strong>Book Journey: </strong>' . $mk['your_book_journey'] . '</p>';
+echo '<p><strong>Places Visited: </strong>' .  $mk['places_visited'] . '</p>';
+
+if ( $post->post_type == 'post' && $post->post_status == 'publish' ) {
+	//http://www.wpbeginner.com/wp-themes/how-to-get-all-post-attachments-in-wordpress-except-for-featured-image/
+	//https://wordpress.stackexchange.com/questions/142163/how-can-i-return-multiple-image-attachments-with-wp-get-attachment-image-src
+	//https://codex.wordpress.org/Template_Tags/get_posts
+	//https://developer.wordpress.org/reference/functions/image_downsize/
+	$attachments = get_posts( array(
+		'post_type' => 'attachment',
+		'posts_per_page' => -1,
+		'post_parent' => $post->ID,
+		'orderby' => 'ID',
+		'order' => 'ASC'
+	) );
+	//            'exclude'     => get_post_thumbnail_id()
+
+	if ( $attachments ) {
+		echo '<div class="amcust-wrap-thumbs">';
+		echo '<div class="hr-title hr-long"><abbr>More Images</abbr></div>';
+		echo '<ul class="ul-thumbnails has-' . count($attachments) . '-thumbs">';
+		foreach ( $attachments as $attachment ) {
+			$class = "";// "post-attachment mime-" . sanitize_title( $attachment->post_mime_type );
+			$thumbimg = wp_get_attachment_image_src( $attachment->ID, 'full', false );
+			if ( $image = image_downsize($attachment->ID, 'thumbnail') ) $thumb = $image;
+			echo '<li>';
+			echo '<a href="' . $thumbimg[0] . '">';
+			echo '<img src="' . $thumb[0] .'">';
+			echo '</a>';
+			echo '</li>';
+		}
+		echo '</ul>';
+		echo '</div>';
+	}
+}
+
+			?>
 
 		<?php endif; ?>
 
+		<?php if($amcust_debug_content) echo '<h6>do_action( \'kleo_after_inner_article_loop\' )</h6>'; ?>
 		<?php do_action( 'kleo_after_inner_article_loop' ); ?>
 
 	</div><!--end article-content-->
